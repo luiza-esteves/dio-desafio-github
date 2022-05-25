6560b398e7b194e7946c1f604e238e4e0dfeffd5
luiza <luizaesteves987@gmail.com>
2022-05-25 16:27:29 -0300

    modulos no js

---
+++ javascript/modulos/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Luíza", 20));
+console.log(mostraCidade("Luíza", "Luziânia"));
+console.log(mostraHobby("Luíza", "Jogar futsal"));
